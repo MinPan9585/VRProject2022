a802bd4fc800b8012993173a256ec5d685667660
MinPan9585 <94884915+MinPan9585@users.noreply.github.com>
2022-10-09 19:39:41 +0800

    add LoadScene function and basic locomotion

---
+++ my project/Assets/scripts/MenuPlant.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPlant : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other) {
+        //print("hit something");
+        if(other.gameObject.CompareTag("hand")){
+            //print("hit hand");
+            SceneManager.LoadScene("Scene1009");
+        }
+    }
+}
